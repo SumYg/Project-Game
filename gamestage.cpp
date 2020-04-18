a21822f398da1f66a53d14c30b2da31029ca4feb
Sum Yeung <sam24890667@gmail.com>
2020-04-19 02:07:36 +0800

    Add function insert_card

--- gamestage.cpp
+++ gamestage.cpp
@@ -3,6 +3,20 @@
 #include <ctime>
 using namespace std;
 
+void insert_card(int number, unsigned index, struct Palyers *palyers) {
+  if (palyers[index].handcards.card == 87)
+    palyers[index].handcards.card = number;
+  else {
+    Node *p = new Node;
+    p->next = NULL;
+    p->card = number;
+    Node *head = &palyers[index].handcards;
+    while (head->next != NULL)
+      head = head->next;
+    head->next = p;
+  }
+}
+
 void gamestage(int n, struct Palyers* palyers, int* order) {
   int option;
   srand(time(NULL));
@@ -17,7 +31,7 @@ void gamestage(int n, struct Palyers* palyers, int* order) {
   }
   for (int i=0;i<6;i++){
     for (int j=0;j<n;j++){
-      
+
       cout<< palyers[order[j]].name << ", choose one of the following option: "<<endl;
       cout<<"Enter 1 to draw a card "<<endl;
       cout<<"Enter 2 to discard a card "<<endl;
@@ -27,5 +41,4 @@ void gamestage(int n, struct Palyers* palyers, int* order) {
         while (cardcheck[random] != 0)
           random=rand()%cards;
         cardcheck[random]=1;
-  
-  
+        insert_card(random, order[j], palyers);
