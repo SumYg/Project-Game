578dcfd8ceacbebea2e4fc2b2345adff857a1d49
SumYg <61381909+SumYg@users.noreply.github.com>
2020-04-19 17:21:01 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -25,7 +25,7 @@ int main() {
   cout << "Please enter the number of players: ";
   cin >> n;
   cout << "Please enter your names seperated by space: " << endl;
-  for (int i; i < n; i++) {
+  for (int i = 0; i < n; i++) {
     cin >> palyers[i].name;
     palyers[i].won = 0;
   }
