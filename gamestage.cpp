6aaa4e9b52a00a0839a396eb9bc8daf66e7e3d53
SumYg <61381909+SumYg@users.noreply.github.com>
2020-04-19 16:17:42 +0800

    Update gamestage.cpp

--- gamestage.cpp
+++ gamestage.cpp
@@ -19,7 +19,7 @@ void insert_card(int number, int index, struct Palyers *palyers) {
 
 void print_hand(int index, Palyers *palyers) {
   cout <<  "Your current hand cards:" << endl;
-  if (palyers[index].tail == NULL) {
+  if (palyers[index].tail != NULL) {
     Node *head = & palyers[index].handcards;
     while (head != NULL) {
       cout << head->card << ' ';
