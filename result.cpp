680cfbed1a4004ca5c9421783b289fe24659bbc4
nathanwooo <61879641+nathanwooo@users.noreply.github.com>
2020-04-29 01:55:46 +0800

    Update result.cpp

--- result.cpp
+++ result.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 using namespace std;
 
 int transform1(string num) {
@@ -27,8 +28,14 @@ int calculate_mark(Node *handcards){
   return point;
 }
 void result(int n, struct Palyers* palyers, int* order) {
+  ofstream fout;
+  fout.open("result.txt");
+  
+  int winner=0;
   for (int i=0;i<n;i++){
     int point=calculate_mark(palyers[order[i]].handcards);
     cout<<palyers[order[i]].name<<"'s hand cards have "<<point<<" points"<<endl;
     if (point==27){
-      
+      cout<<"You win this round!!"<<endl;
+      palyers[order[i]].won+=1;
+    }
