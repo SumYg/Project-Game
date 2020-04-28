04e2b0d100b2e72b145ed2de8d02d0bb8a76141c
nathanwooo <61879641+nathanwooo@users.noreply.github.com>
2020-04-28 22:32:54 +0800

    Update result.cpp

--- result.cpp
+++ result.cpp
@@ -1,3 +1,34 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+int transform1(string num) {
+  // this function is to transform the cards to a score
+  switch (num) {
+    case "A":
+      return 1;
+    case "J":
+      return 11;
+    case "Q":
+      return 12;
+    case "K":
+      return 13;
+    default:
+      return stoi(num);
+  }
+}
+int calculate_mark(Node *handcards){
+  Node *current=handcards;
+  int point=0;
+  while (current!=NULL){
+    score+=transform1(current->hand);
+    current=current->next;
+  }
+  return point;
+}
+void result(int n, struct Palyers* palyers, int* order) {
+  for (int i=0;i<n;i++){
+    int point=calculate_mark(palyers[order[i]].handcards);
+    cout<<palyers[order[i]].name<<"'s hand cards have "<<point<<" points"<<endl;
+    if (point==27){
+      
