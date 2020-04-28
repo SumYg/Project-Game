6e97809e3e1e32ef929ea3dd2dca4c99e655a1e0
SumYg <61381909+SumYg@users.noreply.github.com>
2020-04-28 15:01:48 +0800

    Update gamestage.cpp

--- gamestage.cpp
+++ gamestage.cpp
@@ -79,7 +79,7 @@ void gamestage(int n, struct Palyers* palyers, int* order) {
         cout<<"Enter 2 to discard a card "<<endl;
       }
       cin>>option;
-      while (options !=1 && option !=2 || i==0 && option==2){
+      while (options !=1 && option !=2 || palyers[order[j]].tail == NULL){
         cout<<"Invalid option, please input your option again. "<<endl;
         cout<<"Enter 1 to draw a card "<<endl;
         cout<<"Enter 2 to discard a card "<<endl;
