61cf43db41a09f1785803655dba32b4990c77478
SumYg <61381909+SumYg@users.noreply.github.com>
2020-04-29 16:56:05 +0800

    Update functions.h

--- functions.h
+++ functions.h
@@ -7,6 +7,6 @@ void initialization(int n, Palyers* palyers, int* order);
 
 void gamestage(int n, Palyers* palyers, int* order);
 
-void result(int &n, Palyers* palyers, int* order,int round);
+bool result(int &n, Palyers* palyers, int* order,int round);
 
 #endif
